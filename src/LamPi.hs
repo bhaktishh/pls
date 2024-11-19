ad54642fcc348fa528e764c73d26ee3d225fcd42
Bhakti Shah <bhaktishah@8afbf4bd.st-andrews.ac.uk>
2024-11-19 20:29:08 +0000

    i missed the bus for this

--- src/LamPi.hs
+++ src/LamPi.hs
@@ -53,38 +53,25 @@ module LamPi where
     typeinfer (Lam t_ e) ctx = let var = fresh (map fst ctx) 
                                    t = eval t_ [] in 
         case typeinfer (subst 0 (Var (Free var)) e) ((var, t) : ctx) of 
-            Right t' -> Right (VForall t t')
+            Right t' -> undefined -- somw way to go from Val to (Val -> Val) -- horrible idea: (\x -> subst' var x t')
             _ -> Left "could not check lambda"
     typeinfer (App e e') ctx = case typeinfer e ctx of
-        Right (VForall t t') -> if typeinfer e' ctx == Right t then Right (t' t) else Left "??"
-        _ -> Left "??"
+        Right (VForall t t') -> undefined
+        _ -> undefined
 
-    -- typeinfer :: Expr -> Ctx -> Either String Ty
-    -- typeinfer (Var (Free str)) ctx = case lookup str ctx of
-    --     Just (HasType ty) -> Right ty
-    --     _ -> Left "variable not in context"
-    -- typeinfer (Var _) _ = Left "bound variable in typecheck" -- bruh TODO 
-    -- typeinfer (App e1 e2) ctx = case typeinfer e1 ctx of
-    --     Right (Func t1 t2) -> case typeinfer e2 ctx of
-    --         Right t1' -> if t1 == t1' then Right t2 else Left "ill-typed application"
-    --         _ -> Left "could not infer apply arg"
-    --     _ -> Left "ill typed app"
-    -- typeinfer (Lam ty e) ctx = let var = fresh (map fst ctx) in
-    --     case typeinfer (subst 0 (Var (Free var)) e) ((var, HasType ty) : ctx) of
-    --         Right ty' -> Right (Func ty ty')
-    --         _ -> Left "function body could not be typed"
 
-    -- typecheck :: Expr -> Ctx -> Either String Ty
-    -- typecheck expr ctx = do
-    --     t <- typeinfer expr ctx
-    --     _ <- isType t ctx
-    --     Right t
+    subst' :: String -> Val -> Val -> Val
+    subst' = undefined 
+    -- how would you even do subst' _ x (VLam | VForall) ??
+
 
     subst :: Int -> Expr -> Expr -> Expr
     subst i e (Var (Bound i')) = if i == i' then e else Var (Bound i')
     subst _ _ (Var x) = Var x
     subst i e (App e1 e2) = App (subst i e e1) (subst i e e2)
     subst i e (Lam ty e') = Lam ty (subst (i+1) e e')
+    subst _ _ Star = Star
+    subst i e (Forall ty e') = Forall ty (subst (i+1) e e')
 
     -- quote :: Val -> Expr
     -- quote = quote_ 0
