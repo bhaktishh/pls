c8aecf84e621d894e22faad9bae25232e86b0969
bhakti <bs266@st-andrews.ac.uk>
2025-03-10 16:14:41 +0000

    wau

--- src/LamPiHoasReal.hs
+++ src/LamPiHoasReal.hs
@@ -12,10 +12,11 @@ module LamPiHoasReal where
     -}
 
     import Data.Void
-    import Text.Megaparsec
+    import Text.Megaparsec ( (<|>), many, Parsec, MonadParsec(try, eof), parse )
     import Data.Functor (($>))
     import Text.Megaparsec.Char (char, string, lowerChar, upperChar, alphaNumChar, space)
-
+    import Data.List (elemIndex) 
+    
     -- parser
 
     data PTerm = PStar
@@ -27,6 +28,11 @@ module LamPiHoasReal where
                 deriving (Show, Eq)
 
     type Parser = Parsec Void String
+
+    tmParse :: String -> Tcm PTerm
+    tmParse str = case parse pTerm "" str of 
+        Left _ -> Left "parser error"
+        Right tm -> Right tm 
     
     pSpaces :: Parser a -> Parser a 
     pSpaces p = space *> p <* space
@@ -48,9 +54,6 @@ module LamPiHoasReal where
 
     pArrow :: Parser ()
     pArrow = string "->" $> ()
-
-    pKWForall :: Parser ()
-    pKWForall = string "forall" $> ()
 
     pTerm :: Parser PTerm 
     pTerm = pTerm0 <* eof 
@@ -77,7 +80,7 @@ module LamPiHoasReal where
 
     pForall :: Parser PTerm
     pForall = do
-        _ <- pSpaces pKWForall
+        _ <- pSpaces $ string "forall"
         _ <- pSpaces $ char '('
         vname <- pSpaces pVarStr
         _ <- pSpaces $ char ':'
@@ -90,20 +93,20 @@ module LamPiHoasReal where
 
     data Term = BaseTy String
             | Star 
-            | Forall Term Term 
+            | Forall String Term Term 
             | Var Var 
             | App Term Term 
-            | Lam Term Term 
+            | Lam String Term Term 
             deriving (Show, Eq)
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
     data Var = Bound Int 
             | Free String
             | Quote Int
             deriving (Show, Eq)
 
     data Val = VStar
-              | VForall Val (Val -> Val)
-              | VLam Val (Val -> Val)
+              | VForall String Val (Val -> Val)
+              | VLam String Val (Val -> Val)
               | VNeut Neut
 
 
@@ -116,26 +119,26 @@ module LamPiHoasReal where
     eval :: Term -> Env -> Val 
     eval (BaseTy t) _ = VNeut (NVar (Free t)) 
     eval Star _ = VStar 
-    eval (Forall t t') env = VForall (eval t env) (\x -> eval t' (x : env)) 
+    eval (Forall n t t') env = VForall n (eval t env) (\x -> eval t' (x : env)) 
     eval (Var (Bound i)) env = env !! i
     eval (Var v) _ = VNeut (NVar v)
     eval (App e e') env = case eval e env of 
         VNeut n -> VNeut (NApp n (eval e' env)) 
-        VLam _ f -> f (eval e' env)
-        VForall _ f -> f (eval e' env)
+        VLam _ _ f -> f (eval e' env)
+        VForall _ _ f -> f (eval e' env)
         VStar -> error "applying Star"
-    eval (Lam t e) env = VLam (eval t env) (\x -> eval e (x : env))
+    eval (Lam n t e) env = VLam n (eval t env) (\x -> eval e (x : env))
 
     type Type = Val 
-    type Ctx = [(String, Val)] -- typing context 
+    type Ctx = [(String, Val)] -- typing context
 
     type Tcm a = Either String a
 
     subst :: Int -> Term -> Term -> Term   
     subst i x (Var (Bound j)) = if i == j then x else Var (Bound j)
     subst i x (App e1 e2) = App (subst i x e1) (subst i x e2)
-    subst i x (Lam t e) = Lam (subst i x t) (subst (i+1) x e)
-    subst i x (Forall t t') = Forall (subst i x t) (subst (i+1) x t')
+    subst i x (Lam n t e) = Lam n (subst i x t) (subst (i+1) x e)
+    subst i x (Forall n t t') = Forall n (subst i x t) (subst (i+1) x t')
     subst _ _ x = x
 
     quote :: Val -> Term 
@@ -143,8 +146,8 @@ module LamPiHoasReal where
 
     quote_ :: Int -> Val -> Term
     quote_ _ VStar = Star
-    quote_ i (VForall x f) = Forall (quote_ i x) (quote_ (i+1) (f (VNeut (NVar (Bound i)))))
-    quote_ i (VLam x f) = Lam (quote_ i x) (quote_ (i+1) (f (VNeut (NVar (Bound i)))))
+    quote_ i (VForall n x f) = Forall n (quote_ i x) (quote_ (i+1) (f (VNeut (NVar (Bound i)))))
+    quote_ i (VLam n x f) = Lam n (quote_ i x) (quote_ (i+1) (f (VNeut (NVar (Bound i)))))
     quote_ i (VNeut n) = quoteNeutral i n 
 
     quoteNeutral :: Int -> Neut -> Term
@@ -156,10 +159,57 @@ module LamPiHoasReal where
     unify :: Val -> Val -> Bool
     unify a b = quote a == quote b
 
-    typeinfer :: PTerm -> Ctx -> Tcm Type 
-    typeinfer PStar _ = Right VStar  
-    typeinfer (PApp a b) ctx = undefined 
-    typeinfer (PVar x) ctx = undefined 
-    typeinfer (PLam l x t) ctx = undefined 
-    typeinfer (PForall l x t) ctx = undefined 
-    typeinfer (PType x) ctx = undefined 
+    pTermToTerm :: PTerm -> [String] -> Term
+    pTermToTerm PStar _ = Star
+    pTermToTerm (PApp a b) ctx = App (pTermToTerm a ctx) (pTermToTerm b ctx)
+    pTermToTerm (PVar str) ctx = case elemIndex str ctx of 
+        Just i -> Var (Bound i)
+        Nothing -> Var (Free str)
+    pTermToTerm (PType str) _ = BaseTy str
+    pTermToTerm (PForall str t1 t2) ctx = Forall str (pTermToTerm t1 ctx) (pTermToTerm t2 (str : ctx))
+    pTermToTerm (PLam str t1 t2) ctx = Lam str (pTermToTerm t1 ctx) (pTermToTerm t2 (str : ctx))
+
+    typeinfer :: Term -> Ctx -> Tcm Type
+    typeinfer (BaseTy _) _ = Right VStar  
+    typeinfer Star _ = Right VStar 
+    typeinfer (Forall x p p') ctx = case typeinfer p ctx of 
+        Right VStar -> let
+            t = eval p []
+            p'' = subst 0 (Var (Free x)) p' in 
+                case typeinfer p'' ((x, t) : ctx) of
+                    Right VStar -> Right VStar
+                    _ -> Left "could not infer a type for body of forall"
+        _ -> Left "could not infer a type for argument of forall"
+    typeinfer (Var (Free v)) ctx = case lookup v ctx of 
+        Just t -> Right t
+        Nothing -> Left "free variable not in context" 
+    typeinfer (Var _) _ = Left "bound variable in typeinfer"
+    typeinfer (App e e') ctx = case typeinfer e ctx of 
+        Right (VForall n t t') -> case typeinfer e' ctx of 
+            Right t_ | unify t t_ -> Right $ t' t -- do i need to eval here??
+            _ -> Left "could not infer e' in app"
+        _ -> Left "ill-typed application"
+    typeinfer (Lam x p e) ctx = let
+        tm = subst 0 (Var (Free x)) e 
+        t = eval p [] in 
+            case typeinfer tm ((x, t) : ctx) of 
+                Right t' -> Right $ eval (Forall x (quote t) (quote t')) []
+                _ -> Left "could not infer a type for body of lambda"
+
+    process :: String -> Tcm (Val, Type) 
+    process str = do
+        ptm <- tmParse str 
+        let tm = pTermToTerm ptm []
+        ty <- typeinfer tm  []
+        let val = eval tm [] 
+        pure (val, ty)
+
+    main :: IO () 
+    main = do 
+        inp <- getLine 
+        case process inp of 
+            Left err -> print err 
+            Right (val, ty) -> do
+                print (quote val)
+                print (quote ty)
+                pure () 
